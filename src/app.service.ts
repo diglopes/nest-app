59f1ab034b1ed253c188c385ae3ad12631af7a07
Diego Lopes <diego-chez@hotmail.com>
2022-06-02 22:41:49 -0300

    update message

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): any {
+    Logger.log('Sending a hello world message');
+
     return {
       message: 'Hello World!',
+      version: 2,
       env: {
         PORT: process.env.PORT,
         DATABASE: process.env.DATABASE,
