57ab337b00b908cbf498080e6264155abd01db75
Diego Lopes <diego-chez@hotmail.com>
2022-06-02 22:31:45 -0300

    chore: add deploy script

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return {
+      message: 'Hello World!',
+      env: {
+        PORT: process.env.PORT,
+        DATABASE: process.env.DATABASE,
+      }
+    }
   }
 }
